1a375068637c41e701abb5745b1b5497aa5d0fdc
Josh Merlino <joshua.merlino@gmail.com>
2023-04-05 06:31:09 +0000

    update default veriant

--- Button.tsx
+++ Button.tsx
@@ -2,19 +2,15 @@ import classNames from "classnames";
 import { HTMLAttributes } from "react";
 
 interface ButtonProps {
-	size: "default";
-	variant: "default" | "outlined" | "flat";
+	variant: "glowing" | "outlined" | "flat" | "raised";
 }
 
-export default function Button({ children, className, size = "default", variant = "default", ...props }: Partial<ButtonProps & { type: "button" | "submit" | "reset" }> & HTMLAttributes<HTMLButtonElement>) {
+export default function Button({ children, className, variant = "glowing", ...props }: Partial<ButtonProps & { type: "button" | "submit" | "reset" }> & HTMLAttributes<HTMLButtonElement>) {
 
 	// Styles
-	const sizes = {
-		default: "px-4 h-9 py-1 text-sm"
-	};
-
 	const variants = {
-		default: "bg-primary text-white hover:bg-primary-600 focus:bg-primary-600 active:bg-primary-700 !shadow-primary-800/50 shadow-md hover:shadow-lg",
+		glowing: "bg-primary text-white hover:bg-primary-600 focus:bg-primary-600 active:bg-primary-700 !shadow-primary-800/50 shadow-md hover:shadow-lg",
+		raised: "bg-primary text-white hover:bg-primary-600 focus:bg-primary-600 active:bg-primary-700 !shadow-black/20 shadow-md hover:shadow-lg",
 		flat: "bg-transparent text-primary hover:bg-primary/10 hover:active:bg-primary/20 focus:bg-primary/10",
 		outlined: "bg-transparent text-primary ring-1 ring-primary hover:bg-primary/10 hover:active:bg-primary/20 focus:bg-primary/10"
 	};
@@ -25,12 +21,10 @@ export default function Button({ children, className, size = "default", variant 
 		<button
 			className={ classNames(
 				base,
-				sizes[size],
+				"px-4 h-9 py-1 text-sm",
 				variants[variant],
 				className
 			) }
-			{ ...props }>
-			{children}
-		</button>
+			{ ...props }>{children}</button>
 	);
 }
