2dabc438bf45ea615d27843b240f68a507d96bd1
Josh Merlino <joshua.merlino@gmail.com>
2023-04-05 19:02:50 +0000

    colored checkbox

--- Checkbox.tsx
+++ Checkbox.tsx
@@ -2,7 +2,11 @@ import classNames from "classnames";
 import { useRef } from "react";
 import { MdCheck } from "react-icons/md";
 
-export default function Checkbox({ children, className, ...props }: React.InputHTMLAttributes<HTMLInputElement> & { children?: React.ReactNode; }) {
+interface CheckboxProps {
+	color: "primary" | "error" | "success" | "warn";
+}
+
+export default function Checkbox({ children, className, color = "primary", ...props }: Partial<CheckboxProps> & React.InputHTMLAttributes<HTMLInputElement> & { children?: React.ReactNode; }) {
 
 	// Initialize ref
 	const ref = useRef<HTMLDivElement>(null);
@@ -12,7 +16,12 @@ export default function Checkbox({ children, className, ...props }: React.InputH
 
 	// Styles
 	const container = "group flex items-center font-roboto gap-4 w-full";
-	const checkbox = "peer appearance-none border-2 rounded-sm w-5 h-5 border-gray-500 dark:border-gray-600 checked:!border-primary checked:border-[10px] transition-all after:content[''] after:bg-gray-500/10 after:absolute after:w-12 after:h-12 after:left-1/2 after:top-1/2 after:rounded-full after:-translate-x-1/2 after:-translate-y-1/2 after:pointer-events-none after:-z-[1] after:scale-0 focus:after:scale-100 group-active:after:scale-100 duration-100 after:transition-transform checked:after:bg-primary/10 active:border-gray-600 active:dark:border-gray-500";
+	const checkbox = classNames("peer appearance-none border-2 rounded-sm w-5 h-5 border-gray-500 dark:border-gray-600 checked:border-[10px] transition-all after:content[''] after:bg-gray-500/10 after:absolute after:w-12 after:h-12 after:left-1/2 after:top-1/2 after:rounded-full after:-translate-x-1/2 after:-translate-y-1/2 after:pointer-events-none after:-z-[1] after:scale-0 focus:after:scale-100 group-active:after:scale-100 duration-100 after:transition-transform active:border-gray-600 active:dark:border-gray-500 after:z-10",
+		color === "success" ? "checked:!border-success checked:after:bg-success/10" :
+			color === "error" ? "checked:!border-error checked:after:bg-error/10" :
+				color === "warn" ? "checked:!border-warn checked:after:bg-warn/10" :
+					"checked:!border-primary checked:after:bg-primary/10"
+	);
 	const icon = "m-0.5 text-white scale-0 peer-checked:scale-125 absolute transition-transform pointer-events-none";
 	const label = "select-none text-sm text-gray-800 dark:text-gray-200";
 	
