ed0ffcfd7af4dd256f5b89876bd31e6b3e699dc8
Josh Merlino <joshua.merlino@gmail.com>
2023-04-06 19:34:07 +0000

    fix button variant

--- Button.tsx
+++ Button.tsx
@@ -18,10 +18,11 @@ export default function Button({ children, className, variant = "glowing", waves
 					"bg-primary"
 	) : "bg-white");
 
-	const bgColor = color === "success" ? "bg-success hover:bg-success-600 focus:bg-success-600 active:bg-success-700 shadow-success-800/50" :
-		color === "warn" ? "bg-warn hover:bg-warn-600 focus:bg-warn-600 active:bg-warn-700 shadow-warn-800/50" :
-			color === "error" ? "bg-error hover:bg-error-600 focus:bg-error-600 active:bg-error-700 shadow-error-800/50" :
-				"bg-primary hover:bg-primary-600 focus:bg-primary-600 active:bg-primary-700 shadow-primary-800/50";
+	const bgColor = color === "success" ? "bg-success hover:bg-success-600 focus:bg-success-600 active:bg-success-700 !shadow-success-800/50" :
+		color === "warn" ? "bg-warn hover:bg-warn-600 focus:bg-warn-600 active:bg-warn-700 !shadow-warn-800/50" :
+			color === "error" ? "bg-error hover:bg-error-600 focus:bg-error-600 active:bg-error-700 !shadow-error-800/50" :
+				"bg-primary hover:bg-primary-600 focus:bg-primary-600 active:bg-primary-700 !shadow-primary-800/50";
+	
 	const flatColor = color === "success" ? "text-success hover:bg-success/10 hover:active:bg-success/20 focus:bg-success/10" :
 		color === "warn" ? "text-warn hover:bg-warn/10 hover:active:bg-warn/20 focus:bg-warn/10" :
 			color === "error" ? "text-error hover:bg-error/10 hover:active:bg-error/20 focus:bg-error/10" :
@@ -33,7 +34,7 @@ export default function Button({ children, className, variant = "glowing", waves
 	const variants = {
 		glowing,
 		flat,
-		raised: classNames(glowing, "!shadow-black/20"),
+		raised: classNames(glowing.split(" ").filter(a => !a.includes("shadow-primary") && !a.includes("shadow-error") && !a.includes("shadow-warn") && !a.includes("shadow-success")).join(" "), "!shadow-black/20"),
 		outlined: classNames(flat, "ring-1", color === "success" ? "ring-success" :
 			color === "warn" ? "ring-warn" :
 				color === "error" ? "ring-error" :
