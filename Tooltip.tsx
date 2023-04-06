98b629801eb81b7b80dca1730072d6e3f5158013
Josh Merlino <joshua.merlino@gmail.com>
2023-04-06 04:18:17 +0000

    add tooltip

--- Tooltip.tsx
+++ Tooltip.tsx
@@ -15,7 +15,7 @@ export default function Tooltip({ children, className, anchor = "top", ...props 
 	};
 
 	return (
-		<div className={ classNames("absolute text-white bg-neutral-500 dark:bg-neutral-700 text-xs font-medium rounded-md h-6 flex items-center px-2 pointer-events-none opacity-0 scale-0 group-hover:opacity-100 group-hover:scale-100 transition-all m-2", anchors[anchor], className) }
+		<div className={ classNames("absolute text-white bg-neutral-500 dark:bg-neutral-700 text-xs font-medium rounded-md h-6 flex items-center px-2 pointer-events-none opacity-0 scale-0 group-hover:opacity-100 group-hover:scale-100 transition-all m-2 select-none", anchors[anchor], className) }
 			{ ...props }>{children}</div>
 	);
 }
