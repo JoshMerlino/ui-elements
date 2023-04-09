b5492ca42bb8593ededc503111d59db7e6161826
Josh Merlino <joshua.merlino@gmail.com>
2023-04-09 13:39:58 -0400

    add dense drawer items

--- Drawer/DrawerItem.tsx
+++ Drawer/DrawerItem.tsx
@@ -7,9 +7,10 @@ interface DrawerItemProps {
 	active: boolean;
 	icon: IconType;
 	waves: boolean;
+	size: "default" | "dense"
 }
 
-export default function DrawerItem({ children, icon: Icon, active, waves = true, ...props }: Partial<DrawerItemProps> & HTMLAttributes<HTMLLIElement>) {
+export default function DrawerItem({ children, icon: Icon, active, waves = true, size = "default", ...props }: Partial<DrawerItemProps> & HTMLAttributes<HTMLLIElement>) {
 
 	// Initialize the ref
 	const ref = useRef<HTMLLIElement>(null);
@@ -17,12 +18,12 @@ export default function DrawerItem({ children, icon: Icon, active, waves = true,
 	// Add ripple
 	useRipple(ref, waves, active ? "bg-primary" : "bg-black/50 dark:bg-white/50");
 
-	const base = "flex p-4 gap-2 h-14 items-center rounded-full group transition-colors duration-75";
+	const base = "flex p-4 items-center rounded-full group transition-colors duration-75";
 	const gray = "hover:bg-gray-100 text-gray-700 hover:active:bg-gray-200 dark:text-gray-400 dark:hover:bg-gray-700/50 dark:hover:active:bg-gray-700/80";
 	const primary = "bg-primary/5 hover:bg-primary/10 text-primary hover:active:bg-primary/20";
 
 	return (
-		<li className={ classNames(base, active ? primary : gray) }
+		<li className={ classNames(base, active ? primary : gray, size === "dense" ? "h-12 gap-2" : "h-14 gap-4") }
 			ref={ ref }
 			{ ...props }>
 			{Icon && <Icon className="text-2xl pointer-events-none" />}
