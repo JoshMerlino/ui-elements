88936f000d0f0c6503be6ed3f569ba2b51eecc29
Josh Merlino <joshua.merlino@gmail.com>
2023-04-16 03:25:20 +0000

    add theme toggle to universal resources

---
+++ ThemeToggle.tsx
@@ -0,0 +1,74 @@
+import useEventListener from "@ui-elements/util/useEventListener";
+import iconDark from "assets/apple-touch-icon-dark.png";
+import iconLight from "assets/apple-touch-icon.png";
+import { HTMLAttributes, ReactNode, useEffect, useState } from "react";
+import { BsBrightnessHigh } from "react-icons/bs";
+import { MdOutlineBrightnessAuto, MdOutlineDarkMode } from "react-icons/md";
+
+export type Theme = "DARK" | "LIGHT" | "AUTO";
+
+export default function ThemeToggle({ provider = false, children, ...props }: { provider?: boolean, children?: ReactNode } & HTMLAttributes<HTMLDivElement>): JSX.Element | null {
+
+	// Determine initial state
+	const [ state, setState ] = useState<Theme>("theme" in localStorage ? localStorage.theme === "dark" ? "DARK" : "LIGHT" : "AUTO");
+
+	// Manage theme state
+	useEffect(function() {
+
+		// Determine mode
+		const isDark = state === "DARK" || !("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+		// change body class
+		document.documentElement.classList.toggle("dark", isDark);
+
+		// change meta color
+		document.querySelector("meta[name=theme-color]")?.setAttribute("content", isDark ? "#1a202c" : "#fffff");
+		
+		// change apple icon
+		document.querySelector("link[rel=apple-touch-icon]")?.setAttribute("href", isDark ? iconDark : iconLight);
+		
+	}, [ state ]);
+
+	// Attach keybinds
+	useEventListener("keydown", function(event: KeyboardEvent) {
+		if (event.key !== "F10") return;
+		event.preventDefault();
+		nextState();
+	});
+
+	function nextState() {
+		if (state === "DARK") {
+			localStorage.removeItem("theme");
+			setState("AUTO");
+		}
+		if (state === "AUTO") {
+			localStorage.theme = "light";
+			setState("LIGHT");
+		}
+		if (state === "LIGHT") {
+			localStorage.theme = "dark";
+			setState("DARK");
+		}
+	}
+
+	// If provider, don't render anything
+	if (provider) return null;
+
+	if (children) return <div onClick={ nextState }
+		{ ...props }>
+		{ state === "AUTO" && <MdOutlineBrightnessAuto className="w-6 h-6" /> }
+		{ state === "LIGHT" && <BsBrightnessHigh className="w-6 h-6" /> }
+		{ state === "DARK" && <MdOutlineDarkMode className="w-6 h-6" /> }
+		{children}
+	</div>;
+
+	return (
+		<div className="btn"
+			onClick={ nextState }>
+			{ state === "AUTO" && <MdOutlineBrightnessAuto className="w-6 h-6" /> }
+			{ state === "LIGHT" && <BsBrightnessHigh className="w-6 h-6" /> }
+			{state === "DARK" && <MdOutlineDarkMode className="w-6 h-6" />}
+			<span>Change theme</span>
+		</div>
+	);
+}
