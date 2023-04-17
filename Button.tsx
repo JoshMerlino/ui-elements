84332b1728fe64853cff64183c8722d110d9785f
Josh Merlino <joshua.merlino@gmail.com>
2023-04-17 20:11:12 +0000

    update button colors

--- Button.tsx
+++ Button.tsx
@@ -4,7 +4,7 @@ import useRipple from "./util/useRipple";
 
 export interface ButtonProps {
 	variant: "glowing" | "outlined" | "flat" | "raised";
-	color: "primary" | "error" | "success" | "warn"
+	color: "primary" | "error" | "success" | "warn" | "gray";
 	waves: boolean;
 	size: "default" | "large";
 }
@@ -19,15 +19,17 @@ export default function Button({ children, className, size = "default", variant 
 					"bg-primary"
 	) : "bg-white");
 
-	const bgColor = color === "success" ? "bg-success hover:bg-success-600 focus:bg-success-600 active:bg-success-700 !shadow-success-800/50" :
-		color === "warn" ? "bg-warn hover:bg-warn-600 focus:bg-warn-600 active:bg-warn-700 !shadow-warn-800/50" :
-			color === "error" ? "bg-error hover:bg-error-600 focus:bg-error-600 active:bg-error-700 !shadow-error-800/50" :
-				"bg-primary hover:bg-primary-600 focus:bg-primary-600 active:bg-primary-700 !shadow-primary-800/50";
+	const bgColor = color === "success" ? "bg-success-600 hover:bg-success-700 focus:bg-success-700 active:bg-success-800 !shadow-success-800/50" :
+		color === "warn" ? "bg-warn-600 hover:bg-warn-700 focus:bg-warn-700 active:bg-warn-800 !shadow-warn-800/50" :
+			color === "error" ? "bg-error-600 hover:bg-error-700 focus:bg-error-700 active:bg-error-800 !shadow-error-800/50" :
+				color === "primary" ? "bg-primary-600 hover:bg-primary-700 focus:bg-primary-700 active:bg-primary-800 !shadow-primary-800/50" :
+					"bg-gray-500 hover:bg-gray-600 focus:bg-gray-600 active:bg-gray-700";
 	
 	const flatColor = color === "success" ? "text-success hover:bg-success/10 hover:active:bg-success/20 focus:bg-success/10" :
 		color === "warn" ? "text-warn hover:bg-warn/10 hover:active:bg-warn/20 focus:bg-warn/10" :
 			color === "error" ? "text-error hover:bg-error/10 hover:active:bg-error/20 focus:bg-error/10" :
-				"text-primary hover:bg-primary/10 hover:active:bg-primary/20 focus:bg-primary/10";
+				color === "primary" ? "text-primary hover:bg-primary/10 hover:active:bg-primary/20 focus:bg-primary/10" :
+					"text-gray-500 hover:bg-gray-100 hover:active:bg-gray-200 focus:bg-gray-100";
 	
 	// Styles
 	const glowing = classNames("text-white shadow-md hover:shadow-lg", bgColor);
@@ -39,7 +41,8 @@ export default function Button({ children, className, size = "default", variant 
 		outlined: classNames(flat, "ring-1", color === "success" ? "ring-success" :
 			color === "warn" ? "ring-warn" :
 				color === "error" ? "ring-error" :
-					"ring-primary"),
+					color === "primary" ? "ring-primary" :
+						"ring-gray-500"),
 	};
 
 	const sizes = {
