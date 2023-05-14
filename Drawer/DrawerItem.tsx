6d7416fc868fb356ab9ad5c561e8aa6b6facaee4
Josh Merlino <joshua.merlino@gmail.com>
2023-05-14 05:02:14 +0000

    make better at animations

--- Drawer/DrawerItem.tsx
+++ Drawer/DrawerItem.tsx
@@ -16,14 +16,14 @@ export default function DrawerItem({ children, icon: Icon, active, waves = true,
 	const ref = useRef<HTMLDivElement>(null);
 
 	// Add ripple
-	useRipple(ref, waves, active ? "bg-primary" : "bg-black/50 dark:bg-white/50");
+	useRipple(ref, waves, "group-[.active]:!bg-primary bg-black/50 dark:bg-white/50");
 
-	const base = "flex p-4 items-center rounded-full group transition-colors duration-75";
+	const base = "flex p-4 items-center rounded-full transition-colors";
 	const gray = "hover:bg-gray-100 text-gray-700 hover:active:bg-gray-200 dark:text-gray-400 dark:hover:bg-gray-700/50 dark:hover:active:bg-gray-700/80";
-	const primary = "bg-primary/5 hover:bg-primary/10 text-primary-800 dark:text-primary hover:active:bg-primary/20";
+	const primary = "group-[.active]:bg-primary/5 group-[.active]:hover:bg-primary/10 group-[.active]:text-primary-800 group-[.active]:dark:text-primary group-[.active]:hover:active:bg-primary/20";
 
 	return (
-		<div className={ classNames(base, active ? primary : gray, size === "dense" ? "h-12 gap-2" : "h-14 gap-4") }
+		<div className={ classNames(base, gray, primary, size === "dense" ? "h-12 gap-2" : "h-14 gap-4") }
 			ref={ ref }
 			{ ...props }>
 			{Icon && <Icon className="text-2xl pointer-events-none" />}
