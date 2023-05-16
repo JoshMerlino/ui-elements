d660de8149268324fe72b75410f2cabf286224a8
Josh Merlino <joshua.merlino@gmail.com>
2023-05-16 02:23:28 +0000

    add modal

---
+++ Modal.tsx
@@ -0,0 +1,16 @@
+import classNames from "classnames";
+import { PropsWithChildren, useEffect, useState } from "react";
+
+export default function Modal({ children, open: openProp = false }: PropsWithChildren & { open?: boolean; }) {
+
+	const [ open, setOpen ] = useState(false);
+	useEffect(() => setOpen(openProp), [ openProp ]);
+
+	return (
+		<div className={ classNames("bg-black/25 fixed inset-0 flex isolate items-center transition-opacity", !open && "opacity-0 pointer-events-none") }>
+			<dialog className={ classNames("m-auto flex transition-transform bg-transparent text-inherit rounded-lg", open ? "scale-100" : "scale-75") } open={ open }>
+				{children}
+			</dialog>
+		</div>
+	);
+}
