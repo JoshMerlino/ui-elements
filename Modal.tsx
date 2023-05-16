250e6e5ea54e8d6a4824981bf11e3b766f7d81b8
Josh Merlino <joshua.merlino@gmail.com>
2023-05-16 17:13:06 +0000

    update modal z

--- Modal.tsx
+++ Modal.tsx
@@ -1,14 +1,14 @@
 import classNames from "classnames";
-import { PropsWithChildren, useEffect, useState } from "react";
+import { HTMLAttributes, PropsWithChildren, useEffect, useState } from "react";
 
-export default function Modal({ children, open: openProp = false }: PropsWithChildren & { open?: boolean; }) {
+export default function Modal({ children, open: openProp = false, className }: PropsWithChildren & HTMLAttributes<HTMLDivElement> & { open?: boolean; }) {
 
 	const [ open, setOpen ] = useState(false);
 	useEffect(() => setOpen(openProp), [ openProp ]);
 
 	return (
 		<div className={ classNames("bg-black/25 fixed inset-0 flex isolate items-center transition-opacity z-[500]", !open && "opacity-0 pointer-events-none") }>
-			<dialog className={ classNames("m-auto flex transition-transform bg-transparent text-inherit rounded-lg", open ? "scale-100" : "scale-75") } open={ open }>
+			<dialog className={ classNames("m-auto flex transition-transform bg-transparent text-inherit rounded-lg items-center justify-center", open ? "scale-100" : "scale-75", className) } open={ open }>
 				{children}
 			</dialog>
 		</div>
