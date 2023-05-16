da9c0a7e24ec8199459984a6dd4c24ee349923b9
Josh Merlino <joshua.merlino@gmail.com>
2023-05-16 21:56:12 +0000

    modals could be accessed while closed

--- Modal.tsx
+++ Modal.tsx
@@ -1,15 +1,23 @@
 import classNames from "classnames";
-import { HTMLAttributes, PropsWithChildren, useEffect, useState } from "react";
+import { HTMLAttributes, PropsWithChildren, createRef, useEffect, useState } from "react";
 
-export default function Modal({ children, open: openProp = false, className }: PropsWithChildren & HTMLAttributes<HTMLDivElement> & { open?: boolean; }) {
+export default function Modal({ children, open = false, className }: PropsWithChildren & HTMLAttributes<HTMLDivElement> & { open?: boolean; }) {
 
-	const [ open, setOpen ] = useState(false);
-	useEffect(() => setOpen(openProp), [ openProp ]);
+	const [ isContentVisible, setContentVisible ] = useState(open);
+	
+	const ref = createRef<HTMLDialogElement>();
+	useEffect(function() {
+		if (!ref.current) return;
+		if (open) setContentVisible(true);
+		if (!open) setTimeout(function() {
+			setContentVisible(false);
+		}, 250);
+	}, [ open, ref ]);
 
 	return (
 		<div className={ classNames("bg-black/25 fixed inset-0 flex isolate items-center transition-opacity z-[500]", !open && "opacity-0 pointer-events-none") }>
-			<dialog className={ classNames("m-auto flex transition-transform bg-transparent text-inherit rounded-lg items-center justify-center", open ? "scale-100" : "scale-75", className) } open={ open }>
-				{children}
+			<dialog className={ classNames("m-auto flex transition-transform bg-transparent text-inherit rounded-lg items-center justify-center", open ? "scale-100" : "scale-75 pointer-events-none select-none", className) } ref={ ref }>
+				{isContentVisible && children}
 			</dialog>
 		</div>
 	);
