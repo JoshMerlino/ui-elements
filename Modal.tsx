dbad26cbfcc7639a156f9f69106b61be0af83570
Josh Merlino <joshua.merlino@gmail.com>
2023-05-22 01:47:34 +0000

    add alwaysrender mode

--- Modal.tsx
+++ Modal.tsx
@@ -1,7 +1,7 @@
 import classNames from "classnames";
 import { HTMLAttributes, PropsWithChildren, createRef, useEffect, useState } from "react";
 
-export default function Modal({ children, open = false, className }: PropsWithChildren & HTMLAttributes<HTMLDivElement> & { open?: boolean; }) {
+export default function Modal({ children, open = false, className, alwaysRender }: PropsWithChildren & HTMLAttributes<HTMLDivElement> & { open?: boolean; alwaysRender?: boolean }) {
 
 	const [ isContentVisible, setContentVisible ] = useState(open);
 	
@@ -18,7 +18,7 @@ export default function Modal({ children, open = false, className }: PropsWithCh
 	return (
 		<div className={ classNames("bg-black/40 fixed inset-0 flex isolate items-center transition-opacity z-[500]", !open && "opacity-0 pointer-events-none") }>
 			<dialog className={ classNames("m-auto flex transition-transform bg-transparent text-inherit rounded-lg items-center justify-center", open ? "scale-100" : "scale-75 pointer-events-none select-none", className) } ref={ ref }>
-				{isContentVisible && children}
+				{(isContentVisible || alwaysRender) && children}
 			</dialog>
 		</div>
 	);
